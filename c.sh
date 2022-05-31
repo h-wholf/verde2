0ed175958f206d0cb34aeca29503504688a07eb2
h-wholf21 <kemalmoto.e5@gmail.com>
2022-05-31 07:24:51 +0000

    cfd

--- c.sh
+++ c.sh
@@ -1,8 +1,9 @@
-#!/data/data/com.termux/files/usr/bin/zsh
+#!/usr/bin/zsh
 
 cp apoyos2.db ./descarga/c.db
 cd ./descarga
 cp .sqliterc $HOME/.sqliterc 
-echo "select *from beneficiados"|sqlite3 c.db
+echo "select *from beneficiados;"|sqlite3 c.db
 cd $HOME 
-rm .sqliterc 
+rm .sqliterc
+ 
