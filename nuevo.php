56e611c754e79960c574df72168c3e62dde75a3f
Joaquín osorio Vázquez <75679683+h-wholf@users.noreply.github.com>
2022-06-08 15:53:25 -0500

    Update nuevo.php

--- nuevo.php
+++ nuevo.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="lista.css">
 </head>
-<body calss="cuerpo">
+<body class="cuerpo">
 
 	<ul> 
 		<li style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-facebook"></i></span>
