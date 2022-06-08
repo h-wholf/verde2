3aab716d92969db8429b62413dccf7c89277b048
Joaquín osorio Vázquez <75679683+h-wholf@users.noreply.github.com>
2022-06-08 15:23:34 -0500

    Create nuevo.php

---
+++ nuevo.php
@@ -0,0 +1,21 @@
+</div>
+<CENTER>
+<div class="iso">
+	<ul class="lista"> 
+		<li class="lista2" style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-facebook"></i></span>
+			facebook</a></li>
+			
+		<li style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-whatsapp"></i></span>
+			whatsaap</a></li>
+		<li style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-youtube"></i></span>
+			youtube</a></li>	
+		<li style="--i:6;"> <a href="#"> <span></span>
+	
+	
+	
+	
+	
+	</ul>
+	
+	
+	</div>
