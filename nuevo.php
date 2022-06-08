d63f5fa8a2d950a5a13718bf37e6e272fd935adc
Joaquín osorio Vázquez <75679683+h-wholf@users.noreply.github.com>
2022-06-08 15:52:22 -0500

    Update nuevo.php

--- nuevo.php
+++ nuevo.php
@@ -8,15 +8,15 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="lista.css">
 </head>
-<body>
+<body calss="cuerpo">
 
 	<ul> 
 		<li style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-facebook"></i></span>
 			facebook</a></li>
 			
-		<li style="--i:6;--clr:#25d366"> <a href="#"> <span><i class="fa-brands fa-whatsapp"></i></span>
+		<li style="--i:5;--clr:#25d366"> <a href="#"> <span><i class="fa-brands fa-whatsapp"></i></span>
 			whatsaap</a></li>
-		<li style="--i:6;--clr:#c32aa3"> <a href="#"> <span><i class="fa-brands fa-youtube"></i></span>
+		<li style="--i:4;--clr:#c32aa3"> <a href="#"> <span><i class="fa-brands fa-youtube"></i></span>
 			youtube</a></li>	
 		
 	
