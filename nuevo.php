bde31ad0518e378daddb6e6ca07f5f37991e22a8
Joaquín osorio Vázquez <75679683+h-wholf@users.noreply.github.com>
2022-06-08 15:27:51 -0500

    Update nuevo.php

--- nuevo.php
+++ nuevo.php
@@ -1,8 +1,11 @@
-</div>
-<CENTER>
-<div class="iso">
-	<ul class="lista"> 
-		<li class="lista2" style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-facebook"></i></span>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="lista.css">
+</head>
+<body>
+
+	<ul> 
+		<li style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-facebook"></i></span>
 			facebook</a></li>
 			
 		<li style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-whatsapp"></i></span>
@@ -19,3 +22,4 @@
 	
 	
 	</div>
+	</body>
