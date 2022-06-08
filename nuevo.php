c83dcdb6b6a407422d7f6742d7a1f60d62eeb908
Joaquín osorio Vázquez <75679683+h-wholf@users.noreply.github.com>
2022-06-08 16:09:50 -0500

    Update nuevo.php

--- nuevo.php
+++ nuevo.php
@@ -10,13 +10,13 @@
 </head>
 <body>
 <div class="cuerpo">
-	<ul> 
-		<li style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-facebook"></i></span>
+	<ul class="lista1"> 
+		<li class="lista2" style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-facebook"></i></span>
 			facebook</a></li>
 			
-		<li style="--i:5;--clr:#25d366"> <a href="#"> <span><i class="fa-brands fa-whatsapp"></i></span>
+		<li class="lista2" style="--i:5;--clr:#25d366"> <a href="#"> <span><i class="fa-brands fa-whatsapp"></i></span>
 			whatsaap</a></li>
-		<li style="--i:4;--clr:#c32aa3"> <a href="#"> <span><i class="fa-brands fa-youtube"></i></span>
+		<li class="lista2" style="--i:4;--clr:#c32aa3"> <a href="#"> <span><i class="fa-brands fa-youtube"></i></span>
 			youtube</a></li>	
 		
 	
