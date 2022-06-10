acedcf91a322ce464e6ea26671d2a2d3bf9d7120
Joaquín osorio Vázquez <75679683+h-wholf@users.noreply.github.com>
2022-06-10 09:47:38 -0500

    Update 3.php

--- 3.php
+++ 3.php
@@ -74,25 +74,4 @@
 </div>
 </center>
 </div>
-<CENTER>
-<div class="iso">
-	<ul class="lista"> 
-		<li class="lista2" style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-facebook"></i></span>
-			facebook</a></li>
-			
-		<li style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-whatsapp"></i></span>
-			whatsaap</a></li>
-		<li style="--i:6;--clr:#1877f2"> <a href="#"> <span><i class="fa-brands fa-youtube"></i></span>
-			youtube</a></li>	
-		<li style="--i:6;"> <a href="#"> <span></span>
-	
-	
-	
-	
-	
-	</ul>
-	
-	
-	</div>
 
-</CENTER>
